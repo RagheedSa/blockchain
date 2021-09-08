78591996d31ea6bee91b92d7df02801f96011999
Ragheed <Ragheed.sa@hotmail.com>
2021-09-08 23:16:44 +0300

    Initial Commit

---
+++ src/app/mb/mb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mb',
+  templateUrl: './mb.component.html',
+  styleUrls: ['./mb.component.scss']
+})
+export class MbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
